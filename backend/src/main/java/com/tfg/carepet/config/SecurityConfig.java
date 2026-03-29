d11d6c3e0cfa2c133a71974149534f115c79e6f6
Ainhoa-Rodriguez <ainhoa.rodriguez@goumh.umh.es>
2026-03-29 13:27:31 +0200

    Update SecurityConfig

--- backend/src/main/java/com/tfg/carepet/config/SecurityConfig.java
+++ backend/src/main/java/com/tfg/carepet/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.tfg.carepet.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -11,6 +12,7 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@Order(1)
 public class SecurityConfig {
 
     @Bean
