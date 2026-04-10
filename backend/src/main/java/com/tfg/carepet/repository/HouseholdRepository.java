173caee622c33fdb75a636eaa839c8a144320078
Ainhoa-Rodriguez <ainhoa.rodriguez@goumh.umh.es>
2026-04-10 14:09:27 +0200

    Update HouseholdRepository to add repository anotation

--- backend/src/main/java/com/tfg/carepet/repository/HouseholdRepository.java
+++ backend/src/main/java/com/tfg/carepet/repository/HouseholdRepository.java
@@ -4,9 +4,11 @@ import com.tfg.carepet.model.Household;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface HouseholdRepository extends JpaRepository<Household, Long> {
 
     // Buscar hogares donde el usuario es miembro
