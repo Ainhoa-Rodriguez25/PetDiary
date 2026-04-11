5a75d6ae592162bf190dcfe64fb6e223750e1a42
Ainhoa-Rodriguez <ainhoa.rodriguez@goumh.umh.es>
2026-04-11 11:51:41 +0200

    Update HouseholdRepository
    File modified to rename a method that was confusing

--- backend/src/main/java/com/tfg/carepet/repository/HouseholdRepository.java
+++ backend/src/main/java/com/tfg/carepet/repository/HouseholdRepository.java
@@ -13,7 +13,7 @@ public interface HouseholdRepository extends JpaRepository<Household, Long> {
 
     // Buscar hogares donde el usuario es miembro
     @Query("SELECT h FROM Household h JOIN HouseholdMember hm ON h.id = hm.householdId WHERE hm.userId = :userId")
-    List<Household> findHouseholdsByCreatedByUserId(@Param("userId") Long userId);
+    List<Household> findHouseholdsByUserId(@Param("userId") Long userId);
 
     // Buscar hogares creados por un usuario
     List<Household> findByCreatedByUserId(Long createdBy);
