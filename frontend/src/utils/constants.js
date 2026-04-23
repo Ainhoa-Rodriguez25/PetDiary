c0e5c4a5b8abf61b8f48880fe654872a410d962f
Ainhoa-Rodriguez <ainhoa.rodriguez@goumh.umh.es>
2026-04-23 16:32:23 +0200

    feat: Constants file created with values that do not change

---
+++ frontend/src/utils/constants.js
@@ -0,0 +1,26 @@
+export const APP_NAME = 'CarePet';
+
+export const ROLES = {
+    OWNER: 'OWNER',
+    ADMIN: 'ADMIN',
+    MEMBER: 'MEMBER',
+};
+
+export const SPECIES = {
+    DOG: 'dog',
+    CAT: 'cat',
+};
+
+export const GENDER = {
+    MALE: 'male',
+    FEMALE: 'female',
+    UNKNOWN: 'unknown',
+};
+
+export const MEDICATION_FREQUENCY = {
+    DAILY: 'daily',
+    EVERY_12H: 'every_12h',
+    EVERY_8H: 'every_8h',
+    WEEKLY: 'weekly',
+    AS_NEEDED: 'as_needed',
+}
