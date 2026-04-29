b2bcefbcd5766a96bff9e49b06f679ef1510aede
Ainhoa-Rodriguez25 <ainhoa.rodriguez@goumh.umh.es>
2026-04-29 10:37:41 +0200

    Update tailwind.config.js to fix one problem

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,7 @@
 export default {
     content: [
         "./index.html",
-        "./src/**/*.{js,ts,jsx,tsx}",
+        "./src/**/*.{js,ts,jsx,tsx}",  // ← esta línea es crucial
     ],
     theme: {
         extend: {},
